55fb1cf46fe89e83a9b170321af089b85540bdf8
Hao Fu <jamesfuhao@gmail.com>
2016-02-15 10:08:16 -0800

    directly use PATDroid to parse dex

---
+++ src/fu/hao/trust/dvm/DalvikVM.java
@@ -0,0 +1,7 @@
+package fu.hao.trust.dvm;
+
+public class DalvikVM {
+	int[] heap = new int[8192];
+	int[] object_ref = new int[4];
+
+}
--- src/fu/hao/trust/dvm/Main.java
+++ src/fu/hao/trust/dvm/Main.java
@@ -1,7 +1,17 @@
 package fu.hao.trust.dvm;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipFile;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import patdroid.core.ClassInfo;
+import patdroid.core.MethodInfo;
+import patdroid.core.ReflectionClassDetailLoader;
+import patdroid.dalvik.Instruction;
+import patdroid.smali.SmaliClassDetailLoader;
 
 
 /**
@@ -13,12 +23,31 @@ import org.slf4j.LoggerFactory;
 public class Main {
 	private final static Logger logger = LoggerFactory
 			.getLogger(Main.class);
+	
 	public static void main(String[] args) {
-		if (args.length < 2) {
-			logger.error("Invalid args !!!");
+		// when a class is not loaded, load it with reflection
+		ClassInfo.rootDetailLoader = new ReflectionClassDetailLoader();
+		// pick an apk
+		ZipFile apkFile;
+		try {
+			apkFile = new ZipFile(new File("C:/Users/hao/workspace/Button1/app/app-debug.apk"));
+			// load all classes, methods, fields and instructions from an apk
+			// we are using smali as the underlying engine
+			new SmaliClassDetailLoader(apkFile, true).loadAll();
+			// get the class representation for the MainActivity class in the
+			// apk
+			ClassInfo c = ClassInfo.findClass("de.ecspride.Activity1");
+			// find all methods with the name "onCreate", most likely there is
+			// only one
+			MethodInfo[] m = c.findMethodsHere("onCreate");
+			// print all instructions
+			int counter = 0;
+			for (Instruction ins : m[0].insns) {
+				logger.info("[" + counter + "]" + ins.toString());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		
 	}
 
 }
