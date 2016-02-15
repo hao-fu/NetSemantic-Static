cd33a4dffb8859adc8f80a9a79cf6829700e9b6e
Hao Fu <jamesfuhao@gmail.com>
2016-02-15 09:56:00 -0800

    begin implement vm

---
+++ src/fu/hao/trust/dvm/DexFileFormatTest.java
@@ -0,0 +1,21 @@
+package fu.hao.trust.dvm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DexFileFormatTest {
+	
+	DexFileFormat dex = 
+
+	@Test
+	public void testPrintDexHeader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintDexFile() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/fu/hao/trust/dvm/Main.java
@@ -0,0 +1,24 @@
+package fu.hao.trust.dvm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @ClassName: Main
+ * @Description: TODO
+ * @author: hao
+ * @date: Feb 15, 2016 12:08:30 AM
+ */
+public class Main {
+	private final static Logger logger = LoggerFactory
+			.getLogger(Main.class);
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			logger.error("Invalid args !!!");
+		}
+		
+		
+	}
+
+}
